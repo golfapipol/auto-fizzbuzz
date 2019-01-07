5106618dd87df00a3cdfa4b47b9c21191c9b9a66
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-01-07 17:59:28 +0700

    [Added]: atdd use data from csv format

--- steps.sh
+++ steps.sh
@@ -40,5 +40,8 @@ docker-compose -f docker-compose.deploy.yml -f $backing_service up -d
 docker-compose -f $backing_service run -v $PWD/seed/tearup-acceptance.js:/tmp/tearup.js mongodb mongo mongodb://mongodb:27017/fizzbuzzdb /tmp/tearup.js
 ## Acceptance Test
 newman run atdd/fizzbuzz.postman_collection.json -d atdd/test-data/data.json
+# or need to use data from csv format
+# newman run atdd/fizzbuzz.postman_collection.json -d atdd/test-data/data.csv
+
 ## teardown data
-docker-compose -f $backing_service run -v $PWD/seed/teardown-acceptance.js:/tmp/teardown.js mongodb mongo mongodb://mongodb:27017/insurance_gateway /tmp/teardown.js
+docker-compose -f $backing_service run -v $PWD/seed/teardown-acceptance.js:/tmp/teardown.js mongodb mongo mongodb://mongodb:27017/fizzbuzzdb /tmp/teardown.js
