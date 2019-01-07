30b2590692302d18b61867f2aa267b4910420e43
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-01-07 18:13:33 +0700

    [Added]: docker command for remove container after seed data

--- steps.sh
+++ steps.sh
@@ -10,7 +10,7 @@ cd ..
 # Integration Test
 docker-compose -f $backing_service up -d
 ## tearup
-docker-compose -f $backing_service run \
+docker-compose -f $backing_service run --rm \
     -v $PWD/seed/tearup-integrate.js:/tmp/tearup.js mongodb \
     mongo mongodb://mongodb:27017/fizzbuzzdb /tmp/tearup.js
 
@@ -20,7 +20,7 @@ go test -tags=integration ./... -v && echo "Integration Test Passed"
 cd ..
 
 ## teardown
-docker-compose -f $backing_service run \
+docker-compose -f $backing_service run --rm \
     -v $PWD/seed/teardown-integrate.js:/tmp/teardown.js mongodb \
     mongo mongodb://mongodb:27017/fizzbuzzdb /tmp/teardown.js
 
@@ -37,11 +37,15 @@ docker push $registry/fizzbuzz-service:latest && echo "Push to registry Passed"
 docker-compose -f docker-compose.deploy.yml -f $backing_service up -d
 
 ## tearup data
-docker-compose -f $backing_service run -v $PWD/seed/tearup-acceptance.js:/tmp/tearup.js mongodb mongo mongodb://mongodb:27017/fizzbuzzdb /tmp/tearup.js
+docker-compose -f $backing_service run --rm \
+    -v $PWD/seed/tearup-acceptance.js:/tmp/tearup.js mongodb \
+    mongo mongodb://mongodb:27017/fizzbuzzdb /tmp/tearup.js
 ## Acceptance Test
 newman run atdd/fizzbuzz.postman_collection.json -d atdd/test-data/data.json
 # or need to use data from csv format
 # newman run atdd/fizzbuzz.postman_collection.json -d atdd/test-data/data.csv
 
 ## teardown data
-docker-compose -f $backing_service run -v $PWD/seed/teardown-acceptance.js:/tmp/teardown.js mongodb mongo mongodb://mongodb:27017/fizzbuzzdb /tmp/teardown.js
+docker-compose -f $backing_service run -v --rm \
+    $PWD/seed/teardown-acceptance.js:/tmp/teardown.js mongodb \
+    mongo mongodb://mongodb:27017/fizzbuzzdb /tmp/teardown.js
